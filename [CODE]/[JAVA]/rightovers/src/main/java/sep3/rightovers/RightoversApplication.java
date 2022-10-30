f300c73106dff859814cfa87da7706cc4f9d17b7
Kamil <fischbach.kamil@gmail.com>
2022-10-30 19:38:31 +0100

    Set up of JAVA

---
+++ [CODE]/[JAVA]/rightovers/src/main/java/sep3/rightovers/RightoversApplication.java
@@ -0,0 +1,13 @@
+package sep3.rightovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightoversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RightoversApplication.class, args);
+    }
+
+}
---
+++ [CODE]/[JAVA]/rightovers/src/test/java/sep3/rightovers/RightoversApplicationTests.java
@@ -0,0 +1,13 @@
+package sep3.rightovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightoversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
