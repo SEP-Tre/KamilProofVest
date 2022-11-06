2f017531a322bfab44a657b0aca32910e4643414
chrfoyer <chrfoyer@gmail.com>
2022-11-06 15:24:13 +0100

    Added empty constructor
    Added an empty constructor to the dto without luck

--- [CODE]/[Blazor&WebAPI]/rightovers/Shared/Dtos/OverSimpleFoodPostDto.cs
+++ [CODE]/[Blazor&WebAPI]/rightovers/Shared/Dtos/OverSimpleFoodPostDto.cs
@@ -4,4 +4,15 @@ public class OverSimpleFoodPostDto
 {
     public string Title { get; set; }
     public string Category { get; set; }
+
+    public OverSimpleFoodPostDto()
+    {
+        
+    }
+
+    public OverSimpleFoodPostDto(string title, string category)
+    {
+        Title = title;
+        Category = category;
+    }
 }
